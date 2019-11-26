fe9fc62f9dedf5d6999e15a854eaeacada6e4226
zhengfw <zfw55zfw@sina.com>
2019-11-26 16:18:57 +0800

    acitivi runable

--- src/main/java/com/show/activityworkflow/ActivityworkflowApplication.java
+++ src/main/java/com/show/activityworkflow/ActivityworkflowApplication.java
@@ -1,9 +1,12 @@
 package com.show.activityworkflow;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@ComponentScan(value = {"com.show"})
 public class ActivityworkflowApplication {
 
 	public static void main(String[] args) {
